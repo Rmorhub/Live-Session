98128c55dd707d80b309779e90648eace94d96b4
Rmorhub <rmorphone@gmail.com>
2021-08-25 17:22:13 +0300

    HW-map

--- js/functions/task-5/HW/index.js
+++ js/functions/task-5/HW/index.js
@@ -10,11 +10,36 @@
 //input: number, index (optional) , array (optional)
 //output: number
 
+// const mapArrayElements = (arr, callback) => {
+//   let result = [];
+
+//   for (let index = 0; index < arr.length; index += 1) {
+//     const res = callback(arr[index], index, arr);
+//     if (callback) {
+//       result.push(res);
+//     }
+//   }
+
+//   return result;
+// };
+
+// const array = [10, 5, 4, 99, 10, 22, 6, 111, 222];
+// const callbackFunc = (arrElement, index, entireArr) => {
+//   return arrElement * arrElement;
+// };
+
+// console.log(mapArrayElements(array, callbackFunc));
+
+
+
 const mapArrayElements = (arr, callback) => {
   let result = [];
 
   for (let index = 0; index < arr.length; index += 1) {
-    result.push(callback(arr[index], index, arr));
+    const res = callback(arr[index], index, arr);
+    if (callback) {
+      result.push(res);
+    }
   }
 
   return result;
@@ -22,9 +47,6 @@ const mapArrayElements = (arr, callback) => {
 
 const array = [10, 5, 4, 99, 10, 22, 6, 111, 222];
 const callbackFunc = (arrElement, index, entireArr) => {
-  console.log(
-    `arrElement: ${arrElement}, index: ${index}, eltireArr: ${entireArr}`
-  );
   return arrElement * arrElement;
 };
 

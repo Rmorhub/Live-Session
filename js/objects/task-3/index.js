dd5b92e0ac19a81d071901bb358b858d8e539e34
Rmorhub <rmorphone@gmail.com>
2021-09-01 13:50:15 +0300

    task-3

--- js/objects/task-3/index.js
+++ js/objects/task-3/index.js
@@ -1,30 +1,62 @@
-// ===============================================================================
-// to do
-// написать ф-цию compareObject
-// Input: obj,obj 2 объекта
-// output: boolean
+// input-obj,obj
+// output- boolean
+// 1. compare if  number keys in obj1 !== number keys in obj2=>false
+//2. collect keys to arr
+// 3 iterate  in obj1  and compare keys:values obj1 with keys:values obj1
+// 3.1 if (obj1[key] !== obj2[key]) return false
 
-// true если объекты равны (одинаковые ключи и одинаковые значение ключей)
-// false если объекты не равны
+// // option bad
+// function compareObjects(obj1, obj2) {
+//   const keys1 = Object.keys(obj1);
+//   const keys2 = Object.keys(obj2);
 
+//   if (keys1.length !== keys2.length) {
+//     return false;
+//   }
+
+//   for (let index = 0; index < keys1.length; index += 1) {
+//     const key = keys1[index];
+
+//     const value1 = obj1[key];
+//     const value2 = obj2[key];
+
+//     if (value1 !== value2) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// good option
 function compareObjects(obj1, obj2) {
-  if (Object.entries(obj1).join() !== Object.entries(obj2).join()) {
+  const keys1 = Object.keys(obj1);
+
+  if (keys1.length !== Object.keys(obj2).length) {
     return false;
   }
-  return true;
 
-  //   let firstObj = Object.entries(obj1);
-  //   console.log(firstObj);
-  //   let secondObj = Object.entries(obj2);
-  //   firstObj = firstObj.join();
-  //   console.log(firstObj);
-  //   secondObj = secondObj.join();
-  //   console.log(secondObj);
-  //   if (firstObj !== secondObj) {
-  //     return false;
-  //   }
-  //   return true;
+  return !keys1.some((key) => obj1[key] !== obj2[key]);
+
 }
+
+// function compareObjects(obj1, obj2) {
+//   if (Object.entries(obj1).join() !== Object.entries(obj2).join()) {
+//     return false;
+//   }
+//   return true;
+
+//   //   let firstObj = Object.entries(obj1);
+//   //   console.log(firstObj);
+//   //   let secondObj = Object.entries(obj2);
+//   //   firstObj = firstObj.join();
+//   //   console.log(firstObj);
+//   //   secondObj = secondObj.join();
+//   //   console.log(secondObj);
+//   //   if (firstObj !== secondObj) {
+//   //     return false;
+//   //   }
+//   //   return true;
+// }
 
 // function compareObjects(obj1, obj2) {
 //   // put your code here
@@ -67,3 +99,16 @@ const obj4 = {
 console.log(compareObjects(obj1, obj2)); // ==> false
 console.log(compareObjects(obj2, obj3)); // ==> false
 console.log(compareObjects(obj1, obj4)); // ==> true
+
+
+
+// const obj = {
+//   name: 'Tom',
+//   age: 17,
+// };
+
+//input: Object
+//output:array
+
+// const entries = Object.entries(obj)
+// console.log(entries)

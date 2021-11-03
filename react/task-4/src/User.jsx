e7e68a2fe4983e99550c1cb5c6658bf5757fe674
Rmorhub <rmorphone@gmail.com>
2021-11-03 13:40:20 +0200

    task-4

--- react/task-4/src/User.jsx
+++ react/task-4/src/User.jsx
@@ -1,63 +1,101 @@
 /* eslint-disable class-methods-use-this */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-class User extends React.Component {
-  state = {
-    avatarUrl: null,
-    userName: null,
-    userLocation: null,
-  };
+const User = () => {
+  const [userData, setUserData] = useState(null);
+  const { usersId } = useParams();
 
-  componentDidMount() {
-    console.log('componentDidMount was called');
-
-    this.updateUserData(this.props.match.params.usersId);
-  }
-
-  componentDidUpdate(prevProps) {
-    console.log('componentDidUpdate was called');
-
-    const currentUserId = this.props.match.params.usersId;
-
-    if (currentUserId !== prevProps.match.params.usersId) {
-      this.updateUserData(currentUserId);
-    }
-  }
-
-  updateUserData = userId => {
-    fetch(`https://api.github.com/users/${userId}`)
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${usersId}`)
       .then(responce => {
         if (responce.ok) {
           return responce.json();
         }
       })
-      .then(userData => {
-        this.setState({
-          avatarUrl: userData.avatar_url,
-          userName: userData.name,
-          userLocation: userData.location,
+      .then(user => {
+        setUserData({
+          avatarUrl: user.avatar_url,
+          userName: user.name,
+          userLocation: user.location,
         });
       });
-  };
+  }, [usersId]);
 
-  render() {
-    console.log('render was called');
-
-    const { avatarUrl, userName, userLocation } = this.state;
-
-    if (!avatarUrl || !userName || !userLocation) {
-      return null;
-    }
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatarUrl} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{userName}</span>
-          <span className="user__location">{userLocation}</span>
-        </div>
-      </div>
-    );
+  if (!userData) {
+    return null;
   }
-}
+
+  const { avatarUrl, userName, userLocation } = userData;
+
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatarUrl} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{userName}</span>
+        <span className="user__location">{userLocation}</span>
+      </div>
+    </div>
+  );
+};
+
+// class User extends React.Component {
+//   state = {
+//     avatarUrl: null,
+//     userName: null,
+//     userLocation: null,
+//   };
+
+//   componentDidMount() {
+//     console.log('componentDidMount was called');
+
+//     this.updateUserData(this.props.match.params.usersId);
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     console.log('componentDidUpdate was called');
+
+//     const currentUserId = this.props.match.params.usersId;
+
+//     if (currentUserId !== prevProps.match.params.usersId) {
+//       this.updateUserData(currentUserId);
+//     }
+//   }
+
+//   updateUserData = userId => {
+//     fetch(`https://api.github.com/users/${userId}`)
+//       .then(responce => {
+//         if (responce.ok) {
+//           return responce.json();
+//         }
+//       })
+//       .then(userData => {
+//         this.setState({
+//           avatarUrl: userData.avatar_url,
+//           userName: userData.name,
+//           userLocation: userData.location,
+//         });
+//       });
+//   };
+
+//   render() {
+//     console.log('render was called');
+
+//     const { avatarUrl, userName, userLocation } = this.state;
+
+//     if (!avatarUrl || !userName || !userLocation) {
+//       return null;
+//     }
+//     return (
+//       <div className="user">
+//         <img alt="User Avatar" src={avatarUrl} className="user__avatar" />
+//         <div className="user__info">
+//           <span className="user__name">{userName}</span>
+//           <span className="user__location">{userLocation}</span>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default User;
